ee0cd84777a15a6c7ca2189647a0bab311485c35
bubugigi <guglieborgo@gmail.com>
2025-06-18 07:52:44 -0400

    add go concurrency

---
+++ README.md
@@ -0,0 +1,1 @@
+Usage: go run main.go --startDir ./ --ext .log,.txt --keyword sudo,root,password --output ./result.json
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
   "logscanner/utils"
   "encoding/json"
   "strings"
+  "sync"
 )
 
 type Match struct { 
@@ -18,11 +19,11 @@ type Match struct {
 }
 
 func main() {
-  startDir, exts, keywords, outputFile := handleArguments()
+  startDir, exts, keywords, outputFile, workers := handleArguments()
 
   fmt.Printf("Searching files in '%s' with extention '%v' and keywords '%v'\n", startDir, exts, keywords)
 
-  matches, err := scanFiles(startDir, exts, keywords)
+  matches, err := scanFiles(startDir, exts, keywords, workers)
 
   if err != nil {
     fmt.Fprintf(os.Stderr, "Error scanning: %v\n", err)
@@ -37,83 +38,100 @@ func main() {
 
   if outputFile != "" {
     if !strings.HasSuffix(outputFile, ".json") {
-			outputFile += ".json"
-		}
+      outputFile += ".json"
+    }
 
-		err := os.WriteFile(outputFile, jsonData, 0644)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing to output file: %v\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("Results written to %s\n", outputFile)
+    err := os.WriteFile(outputFile, jsonData, 0644)
+    if err != nil {
+      fmt.Fprintf(os.Stderr, "Error writing to output file: %v\n", err)
+      os.Exit(1)
+    }
+
+    fmt.Printf("Results written to %s\n", outputFile)
   } else {
     fmt.Println(string(jsonData))
   }
 }
 
-func handleArguments() (string, []string, []string, string) {
+func handleArguments() (string, []string, []string, string, int) {
   startDir := flag.String("startDir", "/", "Starting directory where search")
   ext := flag.String("ext", ".log,.csv", "Extention to search")
   key := flag.String("keyword", "", "Keywords to search")
   outputFile := flag.String("output", "", "Path to save the result")
+  workers := flag.Int("workers", 8, "Workers to process files")
   flag.Parse()
 
   exts := utils.SplitAndTrim(*ext)
   keywords := utils.SplitAndTrim(*key)
-
   if len(exts) == 0 || len(keywords) == 0 {
     fmt.Fprintf(os.Stderr, "Error: you must specify both exts and keywords\n")
     os.Exit(1)
   }
 
-  return *startDir, exts, keywords, *outputFile
+  return *startDir, exts, keywords, *outputFile, *workers
 }
 
-func scanFiles(startDir string, exts, keywords []string) ([]Match, error) {
+func scanFiles(startDir string, exts, keywords []string, numWorkers int) ([]Match, error) {
   var matches []Match
+  var matchesMu sync.Mutex
 
+  fileChan := make(chan string, 100)
+  wg := sync.WaitGroup{}
+
+  for i := 0; i<numWorkers; i++ {
+    wg.Add(1)
+    workerId := i
+    go func() {
+      defer wg.Done()
+      for path := range fileChan {
+        fmt.Printf("[Worker %d] Processing file: %s\n", workerId, path)
+        processFile(path, keywords, &matches, &matchesMu)
+      }
+    }()
+  }
   err := filepath.WalkDir(startDir, func(path string, d os.DirEntry, err error) error {
     if err != nil {
       return err
     }
-
-    if d.IsDir() {
+    if d.IsDir() || !utils.HasValidExtention(d.Name(), exts) {
       return nil
     }
-
-    if !utils.HasValidExtention(d.Name(), exts) {
-      return nil
-    }
-
-    file, err := os.Open(path)
-    if err != nil {
-      fmt.Fprintf(os.Stderr, "Error opening the file in the path '%s': %v\n ", path, err)
-      return nil
-    }
-
-    defer file.Close()
-
-    scanner := bufio.NewScanner(file)
-    lineNum := 1
-    for scanner.Scan() {
-      line := scanner.Text()
-      if utils.ContainsKeyword(line, keywords) {
-        matches = append(matches, Match{
-          File: path,
-          Line: lineNum,
-          Text: line,
-        })
-      }
-      lineNum++
-    }
-
-    if err := scanner.Err(); err != nil {
-      fmt.Fprintf(os.Stderr, "Errore durante la lettura di '%s': %v\n", path, err)
-    }
-
+    fileChan <- path
     return nil
   })
 
+  close(fileChan)
+  wg.Wait()
+
   return matches, err
+}
+
+func processFile(path string, keywords []string, matches *[]Match, mu *sync.Mutex) {
+  file, err := os.Open(path)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Error opening the file %s:%v\n", path, err)
+    return
+  }
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+  lineNum := 1
+  for scanner.Scan() {
+    line := scanner.Text()
+    if utils.ContainsKeyword(line, keywords) {
+      mu.Lock()
+      *matches = append(*matches, Match{
+        File: path,
+        Line: lineNum,
+        Text: line,
+      })
+      mu.Unlock()
+    }
+    lineNum++
+  }
+
+  if err := scanner.Err(); err != nil {
+    fmt.Fprintf(os.Stderr, "Error reading %s: %v\n", path, err)
+  }
 }
 
